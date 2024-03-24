4e3e1e3cd098232b705a6630d2f0a7456853cf26
João Fulgencio <joaooctf@gmail.com>
2024-03-24 16:46:12 -0300

    Creating LoginRepository with a method that finds a funcionario by matricula and id and return it without password or null if it's not found.

---
+++ src/main/kotlin/database/repository/LoginRepository.kt
@@ -0,0 +1,26 @@
+package database.repository
+
+import domain.User
+import com.google.cloud.firestore.Firestore
+
+interface LoginRepository {
+    fun findByMatriculaAndPassword(matricula: String, password: String): User?
+}
+class FireStoreLoginRepository(private val db: Firestore): LoginRepository {
+    override fun findByMatriculaAndPassword(matricula: String, password: String) : User? {
+        val funcionariosCollection = db.collection("funcionarios")
+
+        val query = funcionariosCollection.
+        whereEqualTo("matricula", matricula)
+            .whereEqualTo("senha", password)
+            .get()
+            .get()
+
+        return if (!query.isEmpty) {
+            val funcionario = query.documents.firstOrNull()
+            funcionario?.toObject(User::class.java)
+        } else {
+            null
+        }
+    }
+}

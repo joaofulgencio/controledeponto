cd29173245b0cbd481e9e943893135cebf85b3f5
João Fulgencio <joaooctf@gmail.com>
2024-05-17 18:02:59 -0300

    Refactor.

--- src/main/kotlin/domain/Relatorio.kt
+++ src/main/kotlin/domain/Relatorio.kt
@@ -1,3 +1,3 @@
 package domain
 
-data class Relatorio(val email: String = "", val entrada: String = "", val saida: String = "", val dia: String = "")
+data class Relatorio(val email: String = "", val dataDoPonto: String = "", val entrada: String = "", val saida: String = "")
